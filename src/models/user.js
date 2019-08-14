344520676813ee2b3d727b4bba6ae99b8e5d0c11
Eraldo Forgoli <eraldoforgoli@gmail.com>
2019-08-14 17:26:55 +0200

    add password hashing, add user endpoints

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const User = mongoose.model("User", {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
@@ -15,7 +16,31 @@ const User = mongoose.model("User", {
         throw new Error(`${value} is not a valid email!`);
       }
     }
+  },
+  password: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (value.length < 6) {
+        throw new Error("password length > 6");
+      }
+      if (value.includes("password")) {
+        throw new Error("shoul not include password");
+      }
+    },
+    trim: true
   }
 });
+
+userSchema.pre("save", async function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,20 +1,61 @@
 const express = require("express");
 const router = new express.Router();
-const User = require('../models/user')
+const User = require("../models/user");
+
+router.get("", (request, response) => {
+  response.send("Hey");
+});
+
+router.get("/users", async (request, response) => {
+  try {
+    const users = await User.find();
+    response.send(users);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
 
 router.post("/users", async (request, response) => {
-  const user = new User(request.body)
+  const user = new User(request.body);
   try {
     await user.save();
     response.status(200).send(user);
-  } catch(error) {
+  } catch (error) {
     response.status(400).send(error);
   }
 });
 
-router.get('/users', (request, response) => {
-  const user = new User({"name": "Eraldo", "email" : "eraldoforgoli@gmail.com"})
-  response.send(user)
-})
+router.patch("/users/:id", async (request, response) => {
+  const id = request.params.id;
+  const updates = Object.keys(request.body);
+  const allowedUpdates = ["name", "email", "password"];
+  const isValidOperation = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation)
+    return response.status(400).send({ error: "invalid updates" });
+
+  try {
+    const user = await User.findById(request.params.id);
+    updates.forEach(update => (user[update] = request.body[update]));
+    await user.save();
+  } catch (error) {
+    response.status(400).send(error);
+  }
+});
+
+router.delete("/users/:id", async (request, response) => {
+  const id = request.params.id;
+
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return response.status(404).send();
+    }
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
 
 module.exports = router;
